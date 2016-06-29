8d7413bd4bb8465200dc18c846098536e0fb39fc
William Huebler <hueblerw@gmail.com>
2016-06-29 15:46:11 -0500

    Add puppy characteristics in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Doggus Domesticus
+
+# CHARACTERISTICS ----------------
+# Tail activity: waggy
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Lick Face
+# Play with toy
+# Fetch
+# Be Cute
+
