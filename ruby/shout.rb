c68542c37e66282d0578d8b3b417bd82fe3e2a21
William Huebler <hueblerw@gmail.com>
2016-07-03 08:30:02 -0500

    finished release 1, can now yell angrily and happily

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,18 @@
 module Shout
 
+	def self.yell_angrily(words)
+    	words + "!!!" + " :("
+  	end
 
+  	def self.yelling_happily(words)
+  		words.upcase + "!!!  :)"
+  	end
 
 end
+
+# -----------------------
+
+words = "You bought a cat"
+
+p Shout.yell_angrily(words)
+p Shout.yelling_happily(words)
