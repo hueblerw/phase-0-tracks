24d2201d64d505601d96b573dac28eea313b07fc
William Huebler <hueblerw@gmail.com>
2016-07-24 19:30:19 -0500

    created the pretty print methods for both tables

--- databases/train_database/train_database.rb
+++ databases/train_database/train_database.rb
@@ -3,66 +3,98 @@
 require 'sqlite3'
 
 # OK, what's the plan?
-# We need a block of code that will only run to initialize the data from file.  Somehow . . .
-def initialize_the_tables(db)
-	# Create the tables
-	create_table_string = <<-SQL
-  		CREATE TABLE IF NOT EXISTS ho_trains(
-    		id INTEGER PRIMARY KEY, description VARCHAR(1024), engine_num INTEGER, quantity INTEGER,
-    		manufacturer_id INTEGER, DCC_id INTEGER, sound_system_id INTEGER, price_when_new INTEGER,
-    		include_$100_for_Sound_Traxx VARCHAR(255)
-  		)
-		SQL
-	db.execute(create_table_string)
-	create_table_string = <<-SQL
-  		CREATE TABLE IF NOT EXISTS g_scale_trains(
-    		id INTEGER PRIMARY KEY, description VARCHAR(1024), engine_num INTEGER, quantity INTEGER,
-    		manufacturer_id INTEGER, DCC_id INTEGER, sound_system_id INTEGER, price_when_new INTEGER,
-    		include_$100_for_Sound_Traxx VARCHAR(255)
-  		)
-		SQL
-	db.execute(create_table_string)
-	create_table_string = <<-SQL
-  		CREATE TABLE IF NOT EXISTS manufacturers(
-    		id INTEGER PRIMARY KEY, name VARCHAR(255)
-  		)
-		SQL
-	db.execute(create_table_string)
-	create_table_string = <<-SQL
-  		CREATE TABLE IF NOT EXISTS DCC(
-    		id INTEGER PRIMARY KEY, yes_no CHAR(1)
-  		)
-		SQL
-	db.execute(create_table_string)
-	create_table_string = <<-SQL
-  		CREATE TABLE IF NOT EXISTS sound_system(
-    		id INTEGER PRIMARY KEY, name VARCHAR(255)
-  		)
-		SQL
-	db.execute(create_table_string)
-	# Enter the data.
-end
-
-def destroy_the_tables(db)
-
-end
-
-########################################################################
 
 # Then we need a user interface that will allow us to print the data and interact with the data.
 # Requires a print data method.
 # One for HO trains
 
-
-def print_HO(db)
-	ho_trains = db.execute("SELECT * FROM ho_trains")
+def pretty_print_HO(db)
+	create_table_string = <<-SQL
+  		SELECT HO_trains.description, HO_trains.engine_number, HO_trains.quantity, 
+  			manufacturers.name, HO_trains.DCC, sound_system.name, HO_trains.price_when_new, 
+  			HO_trains.include_sound_traxx
+		FROM HO_trains
+		JOIN manufacturers
+		ON HO_trains.manufacturer_id = manufacturers.id
+		JOIN sound_system
+		ON HO_trains.sound_system_id = sound_system.id;
+	SQL
+	ho_trains = db.execute(create_table_string)
+	puts("Description" + "\t\t" + "| Eng #" + "\t" + "| Quant" + "\t" + "| Manufacturer" + "\t\t" + "| DCC" + "\t" + "| Sound" + "\t\t" + "| Price" + "\t" + "| Include Traxx?")
+	puts ""
 	ho_trains.each do |train|
-		puts "#{train[0]} is #{kitten['age']}"
+		i = 0
+		begin
+			if (i == 0)
+				print train[i].to_s + "\t"
+			else
+				print "| " + train[i].to_s + "\t"
+			end
+			if (i == 5 && train[i].to_s.length < 6)
+				print "\t"
+			elsif (i == 3 && train[i].to_s.length < 6)
+				print "\t\t"
+			elsif (i == 3 && train[i].to_s.length < 14)
+				print "\t"
+			elsif (i == 0 && train[i].to_s.length < 8)
+				print "\t\t"
+			elsif (i == 0 && train[i].to_s.length < 16)
+				print "\t"
+			end
+			i += 1
+		end until i == 8
+		print "\n"
 	end
+	puts ""
+	puts "-----------------------------------------------------------------------------------------------"
+	puts ""
 end
-# end
 
 # One for G-scale trains
+
+def pretty_print_G_scale(db)
+	create_table_string = <<-SQL
+  		SELECT G_scale_trains.description, G_scale_trains.engine_number, G_scale_trains.quantity, 
+  			manufacturers.name, G_scale_trains.DCC, sound_system.name, G_scale_trains.price_when_new, 
+  			G_scale_trains.include_sound_traxx
+		FROM G_scale_trains
+		JOIN manufacturers
+		ON G_scale_trains.manufacturer_id = manufacturers.id
+		JOIN sound_system
+		ON G_scale_trains.sound_system_id = sound_system.id;
+	SQL
+	g_scale = db.execute(create_table_string)
+	puts("Description" + "\t\t" + "| Eng #" + "\t" + "| Quant" + "\t" + "| Manufacturer" + "\t\t" + "| DCC" + "\t" + "| Sound" + "\t\t" + "| Price" + "\t" + "| Include Traxx?")
+	puts ""
+	g_scale.each do |train|
+		i = 0
+		begin
+			if (i == 0)
+				print train[i].to_s + "\t"
+			elsif (i == 5 && train[i].to_s.length >= 14)
+				print "| " + train[i].to_s[0 .. 10] + "..."
+			else
+				print "| " + train[i].to_s + "\t"
+			end
+			if (i == 5 && train[i].to_s.length < 6)
+				print "\t"
+			elsif (i == 3 && train[i].to_s.length < 6)
+				print "\t\t"
+			elsif (i == 3 && train[i].to_s.length < 14)
+				print "\t"
+			elsif (i == 0 && train[i].to_s.length < 8)
+				print "\t\t"
+			elsif (i == 0 && train[i].to_s.length < 16)
+				print "\t"
+			end
+			i += 1
+		end until i == 8
+		print "\n"
+	end
+	puts ""
+	puts "-----------------------------------------------------------------------------------------------"
+	puts ""
+end
 
 # Requires a generic add train method for either database.
 
@@ -83,10 +115,5 @@ end
 # Create/Select the database
 db = SQLite3::Database.new("trains.db")
 
-# If necessary re-initialize the database.
-start_fresh = true
-if (start_fresh)
-	File.delete("trains.db")
-	db = SQLite3::Database.new("trains.db")
-	initialize_the_tables(db)
-end
+pretty_print_G_scale(db)
+pretty_print_HO	(db)
