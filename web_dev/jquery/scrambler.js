78e3bcaca2999a72237010f466180e0acfd19006
William Huebler <hueblerw@gmail.com>
2016-07-29 20:30:00 -0500

    got the jquery to work only by using the google link version

--- web_dev/jquery/scrambler.js
+++ web_dev/jquery/scrambler.js
@@ -6,7 +6,11 @@ console.log("This thing is working!!!");
 // Dog's swamp vertically.
 // If we double-click the cat and dog pictures change.
 
+// <script src="C:/Program Files (x86)/ASUS/Giftbox/node_modules/jquery/src/jquery.js"></script>
+
 
 $(document).ready(function() {
-	console.log("WHAAAAAAA!!!!")
+	$('#upperleft').on('click', function() {
+		(this).slideToggle(500);
+	});
 });
