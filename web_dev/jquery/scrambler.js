015fd222b646573bc8e927c841c2620e9735bb44
William Huebler <hueblerw@gmail.com>
2016-07-29 21:33:10 -0500

    cats and dogs now freely switch except for the fact that the one on the right moves when the other switches

--- web_dev/jquery/scrambler.js
+++ web_dev/jquery/scrambler.js
@@ -1,19 +1,31 @@
 console.log("This thing is working!!!");
 
-
-// Ok what we need is a thing that if we click on it swaps the two adjacent pictures.
-// Cat's swap horizontally.
-// Dog's swamp vertically.
-// If we double-click the cat and dog pictures change.
-
 // <script src="C:/Program Files (x86)/ASUS/Giftbox/node_modules/jquery/src/jquery.js"></script>
 
+// If we click the cat and dog pictures change to the other animal.
 
 $(document).ready(function() {
-	$('.cat').on('click', function() {
-		$(this).fadeOut(500)
-           .delay(500)
-           .queue(function(next) { $(this).attr('src','cute_dog.jpg'); next(); })
-           .fadeIn(500); 
+	$('img').on('click', function(){
+		if ($(this).hasClass('cat')){
+			$(this).fadeOut(500)
+           		.delay(500)
+           		.queue(function(next) { 
+           			$(this).attr('src','cute_dog.jpg'); 
+           			next(); 
+           		})
+           		.fadeIn(500); 
+           		$(this).removeClass('cat');
+           		$(this).addClass('dog');
+		} else {
+			$(this).fadeOut(500)
+           		.delay(500)
+           		.queue(function(next) { 
+           			$(this).attr('src','cute_cat.jpg'); 
+           			next(); 
+           		})
+           		.fadeIn(500); 
+           		$(this).removeClass('dog');
+           		$(this).addClass('cat');
+		}
 	});
 });
